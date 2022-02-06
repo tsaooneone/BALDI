9d115e85b0feaabfa63cf75964465bebaaf251f2
tsaooneone <tcw1126one@gmail.com>
2022-02-06 19:01:05 +0800

    220206

---
+++ js/member.js
@@ -0,0 +1,11 @@
+
+var dialog;
+window.onload=function(){
+    dialog=document.getElementById("dialog")
+};
+function showDialog(){
+    dialog.style.display="block";
+};
+function closeDialog(){
+    dialog.style.display="none";
+};
